bedde69a97ba5302785fed4c3e2fcdfe6a47f505
Trey Tomlin <treytomlin91@gmail.com>
2023-04-25 15:08:45 +0000

    begins script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws_scripts
+
+## Thing is a repo used to house scripts used in aws cloud environments
---
+++ scan_security_groups.py
@@ -0,0 +1,3 @@
+# https://gist.github.com/richadams/384020d6e4e6d4f400d7
+# https://hystax.com/how-to-find-all-aws-security-groups-which-allow-unrestricted-traffic-to-avoid-uncontrol-access/
+
