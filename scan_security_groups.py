8ce322514205f3485e831f1a641efb6723df0bd3
Trey Tomlin <treytomlin91@gmail.com>
2023-04-25 15:56:32 +0000

    adds aws doc comment

--- scan_security_groups.py
+++ scan_security_groups.py
@@ -1,3 +1,4 @@
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-security-groups.html
 # https://gist.github.com/richadams/384020d6e4e6d4f400d7
 # https://hystax.com/how-to-find-all-aws-security-groups-which-allow-unrestricted-traffic-to-avoid-uncontrol-access/
 
